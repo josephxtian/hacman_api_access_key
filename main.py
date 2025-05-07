21eed982f692d44a157bc5d93ad84cea4cdd05ca
josephxtian <10685917+josephxtian@users.noreply.github.com>
2025-05-07 17:59:14 +0100

    adds version header

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import csv
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Response, Header
 from pydantic import BaseModel
 
 env = "dev_"
+latest_version = 1.0
 
 app = FastAPI()
 
@@ -11,34 +12,32 @@ class AccessOutput(BaseModel):
     member_id: int
 
 # API check status
-@app.get("/v1.0/status/",status_code=status.HTTP_200_OK)
+@app.get("/status/",status_code=status.HTTP_200_OK)
 def check_api_status():
-    return {"body":"API is online and working"}
+    return {
+        "body":"API is online and working",
+        "latest_version":latest_version
+            }
 
 
-# Doorbot
-@app.get("/v1.0/access/door/fob_id/{fob_id}",
-         status_code = status.HTTP_202_ACCEPTED,
-         response_model=AccessOutput)
-def doorbot_request(fob_id: str):
-    with open(f'{env}door_information.csv','r',) as file:
-        door_list = csv.reader(file)
-        for row in door_list:
-            if fob_id == row[0]:
-                return {"announce_name": row[1], "member_id":row[2]}
-            
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail ="user not on door access list")
-                # The client does not have access rights to the content,
-                # the server is refusing to give the requested resource.
-                # Unlike 401, the client's identity is known to the server.
-
-        # if fob_id in door_list:
-        #     if fob_id 
-    # if the fob_id is accepted 
-    # return status_code=status.HTTP_202_ACCEPTED
-    # and the basemodel
-    # else return negative status_code
+# Doorbot v1.0
+@app.get(
+        "/access/door/fob_id/{fob_id}", status_code = status.HTTP_202_ACCEPTED, response_model=AccessOutput)
+def doorbot_request(fob_id: str, response:Response, version: float = latest_version):
+    response.headers["Version"] = '1.0'
+    if version == 1.0:
+        with open(f'{env}door_information.csv','r',) as file:
+            door_list = csv.reader(file)
+            for row in door_list:
+                if fob_id == row[0]:
+                    return {"announce_name": row[1], "member_id":row[2]}
+                
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                detail ="user not on door access list")
+            # 403 Forbidden
+            # The client does not have access rights to the content,
+            # the server is refusing to give the requested resource.
+            # Unlike 401, the client's identity is known to the server.
 
 
 # toolbot
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,41 +1,57 @@
 from fastapi.testclient import TestClient
 from main import app
+from main import latest_version
 
 client = TestClient(app)
 
 class TestCheckAPIStatus:
     def test_check_api_status(self):
-        response = client.get("/v1.0/status/")
+        response = client.get("/status/")
         assert response.status_code == 200
-        assert response.json() == {"body":"API is online and working"}
+        assert response.json() == {
+        "body":"API is online and working",
+        "latest_version":latest_version
+            }
 
 class TestDoorbot:
 # test with no announce name
     def test_doorbot_without_announce_name(self):
-        response = client.get("/v1.0/access/door/fob_id/ff8d0h4749")
+        response = client.get("/access/door/fob_id/ff8d0h4749")
         assert response.status_code == 202
         assert response.json() == {"announce_name": '', "member_id":663}
 
 # test with announce name
     def test_doorbot_with_announce_name(self):
-        response = client.get("/v1.0/access/door/fob_id/ff8d0h4326")
+        response = client.get("/access/door/fob_id/ff8d0h4326")
         assert response.status_code == 202
         assert response.json() == {"announce_name": 'canene', "member_id":188}
 
 # test with announce emoji
     def test_doorbot_with_emoji_announce_name(self):
-        response = client.get("/v1.0/access/door/fob_id/088dfh4b")
+        response = client.get("/access/door/fob_id/088dfh4b")
         assert response.status_code == 202
         assert response.json() == {"announce_name": '♂️', "member_id":930}
 
 # test with incorrect fob_id
     def test_doorbot_with_false_keyfob(self):
-        response = client.get("/v1.0/access/door/fob_id/953ghf3n")
+        response = client.get("/access/door/fob_id/953ghf3n")
         assert response.status_code == 403
         assert response.json() == {"detail":"user not on door access list"}
 
 # test with partial fob_id
     def test_doorbot_with_false_keyfob(self):
-        response = client.get("/v1.0/access/door/fob_id/088d")
+        response = client.get("/access/door/fob_id/088d")
+        assert response.status_code == 403
+        assert response.json() == {"detail":"user not on door access list"}
+
+# test version 1.0 success
+    def test_doorbot_v1_0(self):
+        response = client.get("/access/door/fob_id/088dfh4b?version=1.0")
+        assert response.status_code == 202
+        assert response.json() == {"announce_name": '♂️', "member_id":930}
+
+# test version 1.0 forbidden
+    def test_doorbot_with_false_keyfob(self):
+        response = client.get("/access/door/fob_id/953ghf3n?version=1.0")
         assert response.status_code == 403
         assert response.json() == {"detail":"user not on door access list"}
